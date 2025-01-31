d4c197a090003142d20a5008eaf47660520f3eaa
laggity <graylog000@alpinesd.org>
2025-01-31 12:13:49 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+input.onSound(DetectedSound.Loud, function () {
+    servos.P0.setAngle(180)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    basic.pause(1000)
+    servos.P0.setAngle(0)
+})
+datalogger.deleteLog()
+servos.P0.setAngle(0)
